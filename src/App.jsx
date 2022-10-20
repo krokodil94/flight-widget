afaafdbbc20f3d9c9085e0aa5871fbbd068faf00
Klemen Stanič <klemenstanic@gmail.com>
2022-10-20 11:30:22 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Table from './components/Table'
+const App = () => {
+  return (
+    <div className="departures">
+      <header>
+        ARRIVALS
+      </header>
+      <Table />
+
+    </div>
+  )
+}
+
+export default App
